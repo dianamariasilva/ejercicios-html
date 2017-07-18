84751b5a3bf1513b77aed22863fbb990d62e9cd3
Diana Maria Silva Valencia <diana.silva.v94@gmail.com>
2017-07-18 14:39:26 -0500

    ejercicio extra

--- ejercicio_extra/js/temperatura.js
+++ ejercicio_extra/js/temperatura.js
@@ -1,6 +1,6 @@
 function convertirCelsius(){
 	var entrada = document.getElementById("farenheit").value;
 	var gradF = ((9/5)*entrada)+32;
-	document.write (gradF);
+	gradF = gradF.toFixed(2);
+	document.getElementById("res").innerHTML=gradF;
 }
-temperatura();
